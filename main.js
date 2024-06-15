e13ce4999f9a72560cb8fb8573838e2466ba73c6
SkylerHope <elpidakouro13@gmail.com>
2024-06-15 16:23:47 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  // Load index.html into the new BrowserWindow.
+  win.loadFile('index.html');
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications to stay open until the user explicitly quits.
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On macOS it is common to re-create a window even after all windows have been closed.
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
