d1fbf228e706121ce8aea65e673f5bc2c3f86853
SkylerHope <elpidakouro13@gmail.com>
2024-08-14 17:44:57 +0300

    rebuilding the project

--- scripts/darkmode.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    var button = document.getElementById("dark-mode-button");
-    var body = document.body;
-
-    button.addEventListener('click', () => {
-    body.classList.toggle("dark-mode");
-    });
-});
