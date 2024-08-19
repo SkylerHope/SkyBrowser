2ce15a89b3210bb5c942d56ed02cef499c3a8c59
SkylerHope <elpidakouro13@gmail.com>
2024-08-19 16:21:04 +0300

    feat: added searxng as search engine

--- scripts/search.js
+++ scripts/search.js
@@ -1,6 +1,6 @@
 document.getElementById('search-form').addEventListener('submit', function(event) {
     event.preventDefault();
     var query = document.getElementById('search-bar').value;
-    var search = 'https://skysearch.website/search?q=' + encodeURIComponent(query) + '&category_general=1&language=auto&time_range=&safesearch=2&theme=simple';
+    var search = 'https://search.rhscz.eu/search?q=' + encodeURIComponent(query) + '&category_general=1&language=all&time_range=&safesearch=2&theme=simple';
     window.location.href = search;
 });
